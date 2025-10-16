282f567d358d6cf675f3d53a7e4b36201b401907
zhangtengfei <2936408477@qq.com>
2025-10-16 11:39:54 +0800

    Initial Commit

---
+++ easyView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  easyView
+//
+//  Created by 张腾飞 on 2025/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ easyView/easyViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  easyViewApp.swift
+//  easyView
+//
+//  Created by 张腾飞 on 2025/10/16.
+//
+
+import SwiftUI
+
+@main
+struct easyViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
